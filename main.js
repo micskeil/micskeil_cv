ae363771606a8cb30697fa9a6a6c08d0410b4d15
micskeil <micskeil@gmail.com>
2020-11-17 12:46:31 +0100

    add portfolio section

---
+++ main.js
@@ -0,0 +1,59 @@
+function closeCover() {
+    document.getElementById("cover").classList.remove("d-block");
+    document.getElementById("cover").classList.add("d-none");
+    
+    document.getElementById("close_cover").classList.remove("d-flex");
+    document.getElementById("close_cover").classList.add("d-none");
+
+    document.getElementById("open_cover").classList.remove("d-none");
+    document.getElementById("open_cover").classList.add("d-flex");
+
+    document.getElementById("open_cv").classList.remove("d-flex");
+    document.getElementById("open_cv").classList.add("d-none");
+
+    document.getElementById("close_cv").classList.remove("d-flex");
+    document.getElementById("close_cv").classList.add("d-none");
+
+};
+
+function openCover() {
+    document.getElementById("cover").classList.remove("d-none");
+    document.getElementById("cover").classList.add("d-block");
+
+    document.getElementById("close_cover").classList.remove("d-none");
+    document.getElementById("close_cover").classList.add("d-flex");
+
+
+    document.getElementById("open_cover").classList.remove("d-flex");
+    document.getElementById("open_cover").classList.add("d-none");
+ 
+    document.getElementById("open_cv").classList.remove("d-none");
+    document.getElementById("open_cv").classList.add("d-flex");
+
+    document.getElementById("close_cv").classList.remove("d-flex");
+    document.getElementById("open_cv").classList.add("d-none");
+};
+
+function openCV() {
+    document.getElementById("cv_full").classList.remove("d-none");
+    document.getElementById("cv_full").classList.add("d-flex");
+
+    document.getElementById("close_cv").classList.remove("d-none");
+    document.getElementById("close_cv").classList.add("d-flex");
+
+    document.getElementById("open_cv").classList.remove("d-flex");
+    document.getElementById("open_cv").classList.add("d-none");
+
+}
+
+function closeCV() {
+    document.getElementById("cv_full").classList.remove("d-flex");
+    document.getElementById("cv_full").classList.add("d-none");
+
+    document.getElementById("open_cv").classList.remove("d-none");
+    document.getElementById("open_cv").classList.add("d-flex");
+
+    document.getElementById("close_cv").classList.remove("d-flex");
+    document.getElementById("close_cv").classList.add("d-none");
+    
+}
